ee3887be496b5a9fecfe76f87c855800b3c6e387
tangj <873933753@qq.com>
2026-04-23 18:43:21 +0800

    add src、@、prttier expo-router ...

---
+++ hanber-app-base/src/app/_layout.js
@@ -0,0 +1,6 @@
+import { Slot } from 'expo-router'
+import request from '@/utils/request'
+
+export default function Layout() {
+  return <Slot />
+}
--- hanber_rn_app/index_del.js
+++ hanber_rn_app/index_del.js
@@ -1,8 +1,8 @@
-import { registerRootComponent } from 'expo';
+import { registerRootComponent } from 'expo'
 
-import App from './App_5_rncmp';
+import App from './App_5_rncmp'
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(App)
